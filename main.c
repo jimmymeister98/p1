4720a871e67d860d535d469c0b6ae084cfeb81fa
Jimmy Peter Neitert <jimmy.neitzert@gmail.com>
2018-10-21 12:59:16 +0200

    Funktioniert, zählt mit pritf bis float und int max

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <time.h>
+#include <stdio.h>
+
+int main (void) {
+
+    int i;
+    float x = 0.0;
+
+
+    for ( i =0 ; i < i+1 ; i++ ) {
+        printf("i = %d\n x = %f",i,x);
+x++;
+    }
+    return 0;
+
+}
